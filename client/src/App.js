aa936e57fb18001a7304b29f90cf2af43d8aaa9a
juan <jpalacio@axletravel.com>
2022-03-22 09:59:34 -0600

    Getting started with MERN

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+import Create from "./Components/Staging/index";
+// We import all the components we need in our app
+
+const App = () => {
+ return (
+   <div>
+     <Routes>
+       <Route exact path="/" element={<Create />} />
+     </Routes>
+   </div>
+ );
+};
+ 
+export default App;
